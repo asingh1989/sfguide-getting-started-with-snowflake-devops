850c462976344a72976a6e6003ef453d93c08c45
Singh, Amrit <amrit_singh@optum.com>
2025-11-01 23:37:31 +0530

    f

--- steps/example/insert_customers.sql
+++ steps/example/insert_customers.sql
@@ -1,4 +1,4 @@
-- Create file "insert_customers.sql" and commit it to your git repo
+-- Create file "insert_customers.sql" and commit it to your git repo
 -- The file should have the following contents:
 INSERT INTO customer VALUES 
   (1, 'Jane', 'Doe'),
