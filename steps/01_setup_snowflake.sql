32f6cb58d2dc978dfa3e031cbc479d4998c19467
Singh, Amrit <amrit_singh@optum.com>
2025-11-26 18:35:43 +0530

    Update 01_setup_snowflake.sql

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -67,7 +67,7 @@ create or ALTER TABLE QUICKSTART__{{environment}}.SILVER.AIRPORTS (
 );
 
 
-CREATE or replace TABLE customer (
+CREATE or replace TABLE QUICKSTART__{{environment}}.SILVER.customers (
   id number not null, 
   first_name varchar, 
   last_name varchar
