7ac2bb48630fc34aba4b23a30a0e9757df8687b4
Singh, Amrit <amrit_singh@optum.com>
2026-01-12 23:43:36 +0530

    dfg

---
+++ ddl/stored_procedures/MY_PROC.sql
@@ -0,0 +1,10 @@
+-- This will update the procedure to the latest definition on every run.
+CREATE OR REPLACE PROCEDURE QUICKSTART_PROD.SILVER.MY_PROC(PARAM_1 VARCHAR)
+RETURNS VARCHAR
+LANGUAGE SQL
+AS
+$$
+BEGIN
+    RETURN 'Hello, ' || PARAM_1;
+END;
+$$;
